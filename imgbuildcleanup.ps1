8fc5e4eb9808a41ba62cc4b7280a66db89905738
Josh-ArchDev <62384434+Josh-ArchDev@users.noreply.github.com>
2024-05-06 13:04:42 -0400

    Update imgbuildcleanup.ps1

--- imgbuildcleanup.ps1
+++ imgbuildcleanup.ps1
@@ -1,13 +1,34 @@
-# Function to log messages to a file
-function LogMessage {
+$LogFilePath = "C:\ImageBuild\imgbuildcleanup.log"
+
+
+# Function to log messages to console and file
+function Write-Log
+{
     param (
         [string]$Message
     )
-    $LogFile = "C:\ImageBuild\imgbuildcleanup_log.txt"
-    $Timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $LogEntry = "$Timestamp - $Message"
-    Add-content -Path $LogFile -Value $LogEntry
+
+    try
+    {
+        # Check if the log file directory exists, create it if not
+        $logDirectory = Split-Path -Path $LogFilePath
+        if (-Not (Test-Path -Path $logDirectory))
+        {
+            New-Item -Path $logDirectory -ItemType Directory
+            Write-Host "Log directory created at: $logDirectory"
+        }
+        # Log message to console and file
+        Write-Host $Message
+        Add-Content -Path $LogFilePath -Value "$(Get-Date) - $Message"
+
+    }
+    catch
+    {
+        write-host "Having issues creating or adding information to the logfile at $LogFilePath"
+    }
 }
+
+
 
 # Function to handle errors
 function HandleError {
@@ -15,13 +36,13 @@ function HandleError {
         [string]$ErrorMessage
     )
     Write-Host "Error: $ErrorMessage"
-    LogMessage "Error: $ErrorMessage"
+    Write-Log "Error: $ErrorMessage"
 }
 
 # Delete all contents within C:\ImageBuild directory
 try {
     Get-ChildItem -Path "C:\ImageBuild" | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
-    LogMessage "Deleted all contents within C:\ImageBuild directory"
+    Write-Log "Deleted all contents within C:\ImageBuild directory"
 } catch {
     HandleError "Failed to delete contents within C:\ImageBuild directory: $_"
 }
@@ -29,10 +50,10 @@ try {
 # Disconnect Z: drive
 try {
     Remove-PSDrive -Name "Z" -ErrorAction Stop
-    LogMessage "Disconnected Z: drive"
+    Write-Log "Disconnected Z: drive"
 } catch {
     HandleError "Failed to disconnect Z: drive: $_"
 }
 
 Write-Host "Script execution completed successfully!"
-LogMessage "Script execution completed successfully!"
+Write-Log "Script execution completed successfully!"
