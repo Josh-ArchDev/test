3e2482aa249c33e890d0bc4bf96b2979a7f28700
Josh Sanderlin <josh.sanderlin@microsoft.com>
2024-05-08 13:43:09 -0400

    Updated download script

--- download.ps1
+++ download.ps1
@@ -34,6 +34,11 @@ Function Write-Log
 try 
 {
     
+    Write-Log "The application binary archive files have been found in the $Source directory"
+    $destination = "C:\ImageBuild"
+    Write-log "Copying application installation binaries from $source directory to the C:\ImageBuild directory"
+    c:\\ImageBuild\\azcopy.exe copy 'https://efa56cc125stg.blob.core.windows.net/genpactapps?sp=rl&st=2024-05-08T16:40:15Z&se=2024-05-16T00:40:15Z&spr=https&sv=2022-11-02&sr=c&sig=6tjv81sxmT%2Be417xVLZjTMprSrAKdFd9rjOKCah3v8M%3D' 'c:\\ImageBuild\\' --recursive=true   
+    Write-Log "Successfully copied all application installation binaries to the C:\ImageBuild directory."
     Write-Log "Finding the required application binary archives"
     # Get all folders in the specified directory
     $folders = Get-ChildItem -Path "C:\ImageBuild" -Directory
@@ -41,12 +46,7 @@ try
     $sortedFolders = $folders | Sort-Object CreationTime -Descending
     # Get the most recently created folder
     $mostRecentFolder = $sortedFolders[0].Name
-    $source = "C:\ImageBuild" + $mostRecentFolder
-    Write-Log "The application binary archive files have been found in the $Source directory"
-    $destination = "C:\ImageBuild"
-    Write-log "Copying application installation binaries from $source directory to the C:\ImageBuild directory"
-    c:\\ImageBuild\\azcopy.exe copy 'https://efa56cc125stg.blob.core.windows.net/genpactapps?sp=rl&st=2024-05-08T16:40:15Z&se=2024-05-16T00:40:15Z&spr=https&sv=2022-11-02&sr=c&sig=6tjv81sxmT%2Be417xVLZjTMprSrAKdFd9rjOKCah3v8M%3D' 'c:\\ImageBuild\\' --recursive=true   
-    Write-Log "Successfully copied all application installation binaries to the C:\ImageBuild directory."
+    $source = "C:\ImageBuild\" + $mostRecentFolder
 }
 catch 
 {
