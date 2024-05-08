686656ec1a9d90650163407319386ba2ab557a63
Josh Sanderlin <josh.sanderlin@microsoft.com>
2024-05-08 12:49:30 -0400

    new scripts

---
+++ azcopy.ps1
@@ -0,0 +1,64 @@
+#######################################################################################################################
+###                                                                                                                 ###
+###    Script Name: azcopy.ps1                                                                                      ###
+###    Script Function: This script is meant to download and install the azcopy utility to allow us to copy files   ### 
+###                     from Azure Blob Storage.                                                                    ###
+###                                                                                                                 ###
+###    Script Usage: This template script does not require any parameters at this time, but if required they can    ###
+###                  be added and called as part of the custom image template process.                              ###
+###                                                                                                                 ###        
+###    Script Version: 1.0                                                                                          ###
+###                                                                                                                 ###
+#######################################################################################################################
+
+# Function to write logs
+Function Write-Log
+{
+    param (
+        [string]$Message
+    )
+
+    try
+    {
+        # Check if the log file directory exists, create it if not
+        $logDirectory = Split-Path -Path $LogFilePath
+        if (-Not (Test-Path -Path $logDirectory))
+        {
+            New-Item -Path $logDirectory -ItemType Directory
+            Write-Host "Log directory created at: $logDirectory"
+        }
+        # Log message to console and file
+        Write-Host $Message
+        Add-Content -Path $LogFilePath -Value "$(Get-Date) - $Message"
+
+    }
+    catch
+    {
+        write-host "Having issues creating or adding information to the logfile at $LogFilePath"
+    }
+}
+
+
+# Azure Image Builder Portal Integration Inline Commands
+
+# Inline command to download and extract AZCopy
+try {
+    # Create a new directory for ImageBuild
+    New-Item -Type Directory -Path 'c:\\' -Name 'ImageBuild' -Force
+    Write-Log -Message "Created directory c:\\ImageBuild"
+
+    # Download the AZCopy utility
+    invoke-webrequest -uri 'https://aka.ms/downloadazcopy-v10-windows' -OutFile 'c:\\ImageBuild\\azcopy.zip'
+    Write-Log -Message "Downloaded azcopy.zip"
+
+    # Extract the downloaded zip file
+    Expand-Archive 'c:\\ImageBuild\\azcopy.zip' 'c:\\ImageBuild'
+    Write-Log -Message "Extracted azcopy.zip"
+
+    # Copy the AZCopy executable to the ImageBuild directory
+    copy-item 'C:\\ImageBuild\\azcopy_windows_amd64_*\\azcopy.exe\\' -Destination 'c:\\ImageBuild'
+    Write-Log -Message "Copied azcopy.exe to c:\\ImageBuild"
+}
+catch {
+    Write-Log -Message "An error occurred: $($_.Exception.Message)"
+}
---
+++ download.ps1
@@ -0,0 +1,74 @@
+# Azure Image Builder Portal Integration Inline Commands
+# Inline command that uses AZCopy to download the archive file and extract to the ImageBuilder directory
+# Use the SAS URL for the <ArchiveSource>
+
+Function Write-Log
+{
+    param (
+        [string]$Message
+    )
+
+    try
+    {
+        # Check if the log file directory exists, create it if not
+        $logDirectory = Split-Path -Path $LogFilePath
+        if (-Not (Test-Path -Path $logDirectory))
+        {
+            New-Item -Path $logDirectory -ItemType Directory
+            Write-Host "Log directory created at: $logDirectory"
+        }
+        # Log message to console and file
+        Write-Host $Message
+        Add-Content -Path $LogFilePath -Value "$(Get-Date) - $Message"
+
+    }
+    catch
+    {
+        write-host "Having issues creating or adding information to the logfile at $LogFilePath"
+    }
+}
+
+
+
+### Try to copy all zip files in the blob storage container ###
+try 
+{
+    Write-log "Copying application installation binaries to the C:\ImageBuild directories"
+    $source = "C:\ImageBuild"
+    $destination = "C:\ImageBuild"
+    c:\\ImageBuild\\azcopy.exe copy 'https://efa56cc125stg.blob.core.windows.net/genpactapps?sp=rl&st=2024-05-08T16:40:15Z&se=2024-05-16T00:40:15Z&spr=https&sv=2022-11-02&sr=c&sig=6tjv81sxmT%2Be417xVLZjTMprSrAKdFd9rjOKCah3v8M%3D' 'c:\\ImageBuild\\' --recursive    
+    Write-Log "Successfully copied all application installation binaries to the C:\ImageBuild directory."
+}
+catch 
+{
+    Write-Log -Message "An error occurred: $($_.Exception.Message)"
+    Exit 1
+}
+
+# Get list of zip files in source directory
+try {
+    $zipFiles = Get-ChildItem -Path $source -Filter *.zip -ErrorAction Stop
+} catch {
+    Write-Log "Failed to get list of zip files: $_"
+    exit 1
+}
+
+# Iterate through each zip file and extract its contents
+foreach ($zipFile in $zipFiles) {
+    try {
+        # Get the name of the zip file without extension
+        $zipFileName = [System.IO.Path]::GetFileNameWithoutExtension($zipFile.FullName)
+        
+        # Extract the contents of the zip file to the destination directory
+        Expand-Archive -Path $zipFile.FullName -DestinationPath $destination -ErrorAction Stop
+        
+        # Log success message
+        Write-Log "Extracted $($zipFile.Name) to $destination"
+    } catch {
+        # Log error message if extraction fails
+        Write-Log "Failed to extract $($zipFile.Name): $_"
+    }
+}
+
+Write-Host "Script execution completed successfully!"
+Write-Log "Script execution completed successfully!"
