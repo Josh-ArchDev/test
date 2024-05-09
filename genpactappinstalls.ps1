3d2abff46a7c9a3d019be35f0f5265a2688a921a
Josh Sanderlin <josh.sanderlin@microsoft.com>
2024-05-09 14:27:44 -0400

    Updated Loggin for genpact app installs.

--- genpactappinstalls.ps1
+++ genpactappinstalls.ps1
@@ -1,38 +1,59 @@
+
+#######################################################################################################################
+###                                                                                                                 ###
+###    Script Name: genpactappinstalls.ps1                                                                          ###
+###    Script Function: This script is meant to be run within a Custom Image Template deployment within Azure       ###
+###                     Virtual Desktop. This script installs the required software for the Genpact EN Use case     ###
+###                     within Walmart.                                                                             ### 
+###                     from Azure Blob Storage.                                                                    ###
+###                                                                                                                 ###
+###    Script Usage: This template script does not require any parameters at this time, but if required they can    ###
+###                  be added and called as part of the custom image template process.                              ###
+###                                                                                                                 ###        
+###    Script Version: 1.0                                                                                          ###
+###                                                                                                                 ###
+#######################################################################################################################
+
+
+
+
+# Set logging 
+ 
 $LogFilePath = "C:\ImageBuild\genpactappinstalls.log"
-
-
-# Function to log messages to console and file
 function Write-Log
 {
-    param (
-        [string]$Message
-    )
+	param (
+		[string]$Message
+	)
 
-    try
-    {
-        # Check if the log file directory exists, create it if not
-        $logDirectory = Split-Path -Path $LogFilePath
-        if (-Not (Test-Path -Path $logDirectory))
-        {
-            New-Item -Path $logDirectory -ItemType Directory
-            Write-Host "Log directory created at: $logDirectory"
-        }
-        # Log message to console and file
-        Write-Host $Message
-        Add-Content -Path $LogFilePath -Value "$(Get-Date) - $Message"
+	try
+	{
+		# Check if the log file directory exists, create it if not
+		$logDirectory = Split-Path -Path $LogFilePath
+		if (-Not (Test-Path -Path $logDirectory))
+		{
+			New-Item -Path $logDirectory -ItemType Directory
+			Write-Host "Log directory created at: $logDirectory"
+		}
+		# Log message to console and file
+		Write-Host $Message
+		Add-Content -Path $LogFilePath -Value "$(Get-Date) - $Message"
 
-    }
-    catch
-    {
-        write-host "Having issues creating or adding information to the logfile at $LogFilePath"
-    }
+	}
+	catch
+	{
+		write-host "Having issues creating or adding information to the logfile at $LogFilePath"
+	}
 }
-
+	
+ 
 #ForcePoint
 
 try {
  
-    Start-Process -filepath 'C:\ImageBuild\FP-22.06\FORCEPOINT-ONE-ENDPOINT-x64.exe' -ArgumentList '/v" /quiet /norestart"' -Wait -ErrorAction Stop
+    Write-Log "Starting the ForcePoint Install"
+	Start-Process -filepath 'C:\ImageBuild\FP-22.06\FORCEPOINT-ONE-ENDPOINT-x64.exe' -ArgumentList '/v" /quiet /norestart"' -Wait -ErrorAction Stop
+	Write-Log "Successfully installed the ForcePoint Software"
     #Start-Sleep -Seconds 300
 }
  
@@ -41,14 +62,15 @@ catch {
     $ErrorMessage = $_.Exception.message
  
     write-log "Error installing ForcePoint-v22.06.5578: $ErrorMessage"
+	Exit 42
  
 }
 
 #Tanium
 
 try {
-        $destination = "C:\Temp"
-        $source = "C:\ImageBuild\Tanium 7.4.7.1179\tanium-init.dat"
+		Write-Log "Starting the Tanium Install"    
+		$source = "C:\ImageBuild\Tanium 7.4.7.1179\tanium-init.dat"
     
         Start-Process -FilePath 'C:\ImageBuild\Tanium 7.4.7.1179\TaniumInstall.cmd' -Wait -PassThru -ErrorAction Stop
         Start-Process -FilePath 'C:\Program Files (x86)\Tanium\Tanium Client\TaniumClient.exe' -ArgumentList "config remove ComputerID" -Wait -ErrorAction Stop
@@ -60,6 +82,7 @@ try {
         Stop-Service 'Tanium Client'
         Remove-Item -Path 'C:\Program Files (x86)\Tanium\Tanium Client\pki.db' -Force
         Copy-Item -Path $source -Destination 'C:\Program Files (x86)\Tanium\Tanium Client' 
+		Write-Log "Successfully Completed the Tanium Software Install"   
 
 	}
 
@@ -68,13 +91,36 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing ForcePoint-v22.06.5578: $ErrorMessage"
+		Exit 42
+
+	}
+
+#Trellix Proxy Agent
+
+try {
+
+	Write-Log "Starting the Trellix Proxy Agent Install"	
+	Start-Process -FilePath "C:\ImageBuild\FramePkg581L.exe" -ArgumentList "/install=agent /silent" -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the Trellix Proxy Agent Install"
+
+}
+
+	catch {
+
+		$ErrorMessage = $_.Exception.message
+ 
+    	write-log "Error installing TrellixProxyAgent: $ErrorMessage"
+		Exit 42
 
 	}
 
 #Frameworks
+
 try {
 
+		Write-Log "Starting the install of Visual C++ Redist x64 Package"
 		Start-Process -FilePath "c:\ImageBuild\Frameworks\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait -ErrorAction Stop
+		Write-Log "Successfully Completed the install of Visual C++ Redist x64 Package"
 
 }
 
@@ -83,12 +129,15 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing VC_redist.x64.exe: $ErrorMessage"
+		Exit 42
 
 	}
 
 try {
 
-		Start-Process -FilePath "c:\ImageBuild\Frameworks\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait -ErrorAction Stop
+	Write-Log "Starting the install of Visual C++ Redist x86 Package"	
+	Start-Process -FilePath "c:\ImageBuild\Frameworks\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the install of Visual C++ Redist x64 Package"
 
 }
 
@@ -97,12 +146,15 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing VC_redist.x86.exe: $ErrorMessage"
+		Exit 42
 
 	}
 
 try {
 
-		Add-WindowsCapability -Online -Name NetFx3 -Source "C:\ImageBuild\Frameworks\dotnetfx35.exe"
+	Write-Log "Starting the install of the .Net 3.5 Package"	
+	Add-WindowsCapability -Online -Name NetFx3 -Source "C:\ImageBuild\Frameworks\dotnetfx35.exe"
+	Write-Log "Successfully Completed the install of the .Net 3.5 Package"	
 
 }
 
@@ -111,6 +163,7 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing dotnetfx35: $ErrorMessage"
+		Exit 42
 
 	}
 
@@ -118,8 +171,10 @@ try {
 
 try {
  
-    Start-Process -filepath 'C:\ImageBuild\NewTeams\teamsbootstrapper (1).exe' -ArgumentList '-p -o "c:\ImageBuild\NewTeams\MSTeams-x64.msix"' -Wait -ErrorAction Stop
+    Write-Log "Starting the install of the New Teams Client Package"	
+	Start-Process -filepath 'C:\ImageBuild\Microsoft_Teams_ver24004.1309.2689.2246\Deploy-Application.exe' -ArgumentList 'Install NonInteractive' -Wait -ErrorAction Stop
     #Start-Sleep -Seconds 300
+	Write-Log "Successfully Completed the install of the New Teams Client Package"
 }
  
 catch {
@@ -127,13 +182,14 @@ catch {
     $ErrorMessage = $_.Exception.message
  
     write-log "Error installing NewTeams: $ErrorMessage"
+	Exit 42
  
 }
 
 #SBOPAnalysisMSOffice
 
 try {
-
+		Write-Log "Starting the install of the SBOPAnalysisMSOffice Package"	
 		Start-Process -FilePath "C:\ImageBuild\SBOPAnalysisMSOffice\SBOPAnalysisMSOffice.EXE" -ArgumentList "/S /NOREBOOT" -ErrorAction Stop
 
 		$folderPath = "C:\Windows\PackageLogs\SBOPAnalysisMSOffice"
@@ -160,6 +216,7 @@ try {
         	#Write-Output "$folderPath does not exist"
     }
     		Start-Sleep -Seconds 5
+			Write-Log "Successfully Completed the install of the SBOPAnalysisMSOffice Package"
 }		
 
 }
@@ -169,14 +226,16 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing SBOPAnalysisMSOffice: $ErrorMessage"
+		Exit 42
 
 	}
 
 #Alteryx
 
 try {
-
+		Write-Log "Starting the install of the Alteryx Package"
 		Start-Process -FilePath "C:\ImageBuild\Alteryx_64bit_ver[2019.4g]\Deploy-Application.exe" -ArgumentList "Install NonInteractive" -Wait -ErrorAction Stop
+		Write-Log "Successfully Completed the install of the Alteryx Package"
 
 }
 
@@ -185,6 +244,7 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing Alteryx_2019.4g: $ErrorMessage"
+		Exit 42
 
 	}
 
@@ -192,7 +252,9 @@ try {
 
 try {
 
+		Write-Log "Starting the install of the Reflection MultiHost Package"
 		Start-Process -FilePath "C:\ImageBuild\Reflection\Reflection_Multi_Host.EXE" -ArgumentList "/S /NOREBOOT" -Wait -ErrorAction Stop
+		Write-Log "Successfully Completed the install of the Reflection MultiHost Package"
 
 }
 
@@ -201,22 +263,7 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing ReflectionMultiHost: $ErrorMessage"
-
-	}
-
-#Notepad++ v7.7.1
-
-try {
-
-		Start-Process -FilePath "C:\ImageBuild\Notepad++_ver``[7.7.1``]\Deploy-Application.exe" -ArgumentList "Install NonInteractive" -Wait -ErrorAction Stop
-
-}
-
-	catch {
-
-		$ErrorMessage = $_.Exception.message
- 
-    	write-log "Error installing Notepad++_ver[7.7.1]: $ErrorMessage"
+		Exit 42
 
 	}
 
@@ -224,7 +271,9 @@ try {
 
 try {
 
+		Write-Log "Starting the install of the DocumentDirect Package"	
 		Start-Process -FilePath "C:\ImageBuild\DocumentDirect\documentdirect.EXE" -ArgumentList "/S /NOREBOOT" -Wait -ErrorAction Stop
+		Write-Log "Successfully Completed the install of the DocumentDirect Package"
 
 }
 
@@ -233,6 +282,7 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing DocumentDirect4.4: $ErrorMessage"
+		Exit 42
 
 	}
 
@@ -240,7 +290,9 @@ try {
 
 try {
 
+		Write-Log "Starting the install of the SAP Client GUI Package"
 		Start-Process -FilePath "C:\ImageBuild\SAP_Client_GUI_64bit_ver``[770c``]\Deploy-Application.exe" -ArgumentList "Install NonInteractive" -Wait -ErrorAction Stop
+		Write-Log "Successfully Completed the install of the SAP Client GUI Package"
 
 }
 
@@ -249,6 +301,7 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing SAP_Client_GUI_64bit_ver[770c]: $ErrorMessage"
+		Exit 42
 
 	}		
 
@@ -256,7 +309,9 @@ try {
 
 try {
 
+		Write-Log "Starting the install of the SAP End User INI Package"	
 		Start-Process -FilePath "C:\ImageBuild\SAPGUI ENDUSER INI_ver``[0.0.80``]\sapgui_ini_enduser-6.7.msi" -ArgumentList "/quiet" -Wait -ErrorAction Stop
+		Write-Log "Starting the install of the SAP End User INI GUI Package"
 
 }
 
@@ -265,12 +320,13 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing SAPGUI ENDUSER INI_ver[0.0.80]: $ErrorMessage"
+		Exit 42
 
 	}
 #RightNowService
 
 try {
-
+		Write-Log "Starting the install of the Right Now Service Package"
 		Start-Process -FilePath "C:\ImageBuild\RightNowService\RightNowService.EXE" -ArgumentList "/S /NOREBOOT" -ErrorAction Stop
 
 		$folderPath = "C:\Windows\PackageLogs\RightNowService"
@@ -293,6 +349,7 @@ try {
         	#Write-Output "$folderPath does not exist"
     }
     		Start-Sleep -Seconds 5
+			Write-Log "Successfully Completed the install of the Right Now Service Package"
 }	
 
 }
@@ -302,6 +359,7 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing RightNowService: $ErrorMessage"
+		Exit 42
 
 	}
 
@@ -310,7 +368,10 @@ try {
 
 	try {
 
-		Start-Process msiexec.exe -ArgumentList '/i c:\imagebuild\Chrome\googlechromestandaloneenterprise64.msi /qn' -Wait -ErrorAction Stop
+			Write-Log "Starting the install of the Google Chrome Package"
+			Start-Process msiexec.exe -ArgumentList '/i c:\imagebuild\Chrome\googlechromestandaloneenterprise64.msi /qn' -Wait -ErrorAction Stop
+			Write-Log "Successfully Completed the install of the Google Chrome Package"
+
 
 }
 
@@ -319,6 +380,7 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing Chromev120: $ErrorMessage"
+		Exit 42
 
 	}
 
@@ -326,7 +388,9 @@ try {
 
 try {
 
+		Write-Log "Starting the install of the Bogle Font Package"	
 		Start-Process -FilePath "C:\ImageBuild\BogleFont\BogleFont-1.0.msi" -ArgumentList "/q ALLUSERS=2 /m MSIYXKCO /l* c:\Windows\temp\BogleFonts.log" -Wait -ErrorAction Stop
+		Write-Log "Successfully Compeleted the install of the Bogle Font Package"
 
 }
 
@@ -335,5 +399,6 @@ try {
 		$ErrorMessage = $_.Exception.message
  
     	write-log "Error installing BogleFont: $ErrorMessage"
+		Exit 42
 
 	}
