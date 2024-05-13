e976597ae7ff975c55a3a2dcde022768d02ee838
Josh Sanderlin <josh.sanderlin@microsoft.com>
2024-05-13 12:45:17 -0400

    Updated

--- MoveFSLogixRules.ps1
+++ MoveFSLogixRules.ps1
@@ -4,29 +4,89 @@
 ### our AVD templates.                                                                   ###
 ###                                                                                      ###
 ############################################################################################
+
+# Logifle Path
+$LogFilePath = "C:\Windows\Temp\MoveFSLogixRules.log"
+# Function to write logs
+Function Write-Log
+{
+    param (
+        [string]$Message
+    )
+
+    try
+    {
+        # Check if the log file directory exists, create it if not
+        $logDirectory = Split-Path -Path $LogFilePath
+        if (-Not (Test-Path -Path $logDirectory))
+        {
+            New-Item -Path $logDirectory -ItemType Directory
+            Write-Host "Log directory created at: $logDirectory"
+        }
+        # Log message to console and file
+        Write-Host $Message
+        Add-Content -Path $LogFilePath -Value "$(Get-Date) - $Message"
+
+    }
+    catch
+    {
+        write-host "Having issues creating or adding information to the logfile at $LogFilePath"
+    }
+}
+
+
+
 # Define source and destination directories
 $sourcePath = "c:\Program Files\FSLogix\Apps\Rules\rules"
 $destinationPath = "c:\Program Files\FSLogix\Apps\Rules"
 
 # Check if the source directory exists
-if (Test-Path $sourcePath) {
-    # Get all files in the source directory
-    $files = Get-ChildItem -Path $sourcePath
-
-    # Move each file to the destination directory
-    foreach ($file in $files) {
-        # Define the full destination path for the file
-        $destinationFilePath = Join-Path -Path $destinationPath -ChildPath $file.Name
-
-        # Move the file to the destination directory
-        Move-Item -Path $file.FullName -Destination $destinationFilePath
+Try
+{
+    Write-Log "Moving the FSLogix Rules to the proper directory"
+    if (Test-Path $sourcePath) {
+        # Get all files in the source directory
+        $files = Get-ChildItem -Path $sourcePath
+    
+        # Move each file to the destination directory
+        foreach ($file in $files) {
+            # Define the full destination path for the file
+            $destinationFilePath = Join-Path -Path $destinationPath -ChildPath $file.Name
+    
+            # Move the file to the destination directory
+            Move-Item -Path $file.FullName -Destination $destinationFilePath
+        }
+    
+        # Output completion message
+        Write-Log "All files have been moved from $sourcePath to $destinationPath."
+    } else {
+        # Output error message if the source directory does not exist
+       Write-Log "The source directory $sourcePath does not exist."
     }
+    
+}
+Catch
+{
+    $ErrorMessage = $_.Exception.message
+    write-log "Error Copying the FSLogix Rules Files: $ErrorMessage"
+	Exit 100
 
-    # Output completion message
-    Write-Host "All files have been moved from $sourcePath to $destinationPath."
-} else {
-    # Output error message if the source directory does not exist
-    Write-Host "The source directory $sourcePath does not exist."
+}
+
+### Try to unregister the Scheduled Task ###
+Try
+{
+    Write-Log "Unregistering the scheduled task that called this script"
+    Unregister-ScheduledTask -TaskName "MoveFSLogixRules" -Confirm:$false
+    Write-Log "Successfully unregistered the MoveFSLogixRules Scheduled Task"
+
+}
+Catch
+{
+    $ErrorMessage = $_.Exception.Message
+    Write-Log "Error unregistering the Scheduled Task: $ErrorMessage"
+    Write-Log "This error is non critical and the process will continue"
+    Exit 0
 }
 
 # End of script
