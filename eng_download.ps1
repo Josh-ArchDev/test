5df84bb21ab90617a208478f662e5ab0eca62f11
Josh Sanderlin <josh.sanderlin@microsoft.com>
2024-06-10 13:44:00 -0400

    Adding a new use case for AVD engineering team.

---
+++ eng_download.ps1
@@ -0,0 +1,102 @@
+#######################################################################################################################
+###                                                                                                                 ###
+###    Script Name: eng_download.ps1                                                                            ###
+###    Script Function: This script is meant to download and extract contents of zip files from a Blob Storage      ###
+###                     Account using AZCopy.exe that is downloaded and extracted in the azcopy.ps1 script.         ### 
+###                     This script is used as part of the Custom Image Template deployments for AVD. Once all the  ###
+###                     zip files have been downloaded from ther Blob Store the contents will then be extracted to  ###
+###                     install software for the Custom Image build.                                                ### 
+###                                                                                                                 ###
+###                                                                                                                 ###
+###    Script Usage: This script does not require any parameters at this time, but if required they can             ###
+###                  be added and called as part of the custom image template process.                              ###
+###                                                                                                                 ###        
+###    Script Version: 1.0                                                                                          ###
+###                                                                                                                 ###
+#######################################################################################################################
+
+# Set the Log File Path
+$LogFilePath = "C:\ImageBuild\download.log"
+
+# Function to write logs
+function Write-Log
+{
+	param (
+		[string]$Message
+	)
+
+	try
+	{
+		# Check if the log file directory exists, create it if not
+		$logDirectory = Split-Path -Path $LogFilePath
+		if (-Not (Test-Path -Path $logDirectory))
+		{
+			New-Item -Path $logDirectory -ItemType Directory
+			Write-Host "Log directory created at: $logDirectory"
+		}
+		# Get current date and time
+		$dateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+		# Log message with date and time to console and file
+		$logMessage = "$dateTime - $Message"
+		Write-Host $logMessage
+		Add-Content -Path $LogFilePath -Value $logMessage
+	}
+	catch
+	{
+		write-host "Having issues creating or adding information to the logfile at $LogFilePath"
+	}
+}
+
+
+### Try to copy all zip files in the blob storage container ###
+try 
+{
+    
+    Write-Log "The application binary archive files have been found in the $Source directory"
+    $destination = "C:\ImageBuild"
+    Write-log "Copying application installation binaries from $source directory to the C:\ImageBuild directory"
+    ### Update the <Archive Location> with the SAS token for the proper download of the app binaries ###
+    c:\\ImageBuild\\azcopy.exe copy 'https://teams.microsoft.com/l/message/19:3c284c02-1e0d-4af5-b201-1e9584d19a05_789d758b-61c4-4822-adae-00c312783626@unq.gbl.spaces/1717691738907?context=%7B%22contextType%22%3A%22chat%22%7D' 'c:\\ImageBuild\\' --recursive=true   
+    Write-Log "Successfully copied all application installation binaries to the C:\ImageBuild directory."
+    Write-Log "Finding the required application binary archives"
+    # Get all folders in the specified directory
+    $folders = Get-ChildItem -Path "C:\ImageBuild" -Directory
+    # Sort folders by creation time in descending order
+    $sortedFolders = $folders | Sort-Object CreationTime -Descending
+    # Get the most recently created folder
+    $mostRecentFolder = $sortedFolders[0].Name
+    $source = "C:\ImageBuild\" + $mostRecentFolder
+}
+catch 
+{
+    Write-Log -Message "An error occurred: $($_.Exception.Message)"
+    Exit 1
+}
+
+# Get list of zip files in source directory
+try {
+    $zipFiles = Get-ChildItem -Path $source -Filter *.zip -ErrorAction Stop
+} catch {
+    Write-Log "Failed to get list of zip files: $_"
+    exit 1
+}
+
+# Iterate through each zip file and extract its contents
+foreach ($zipFile in $zipFiles) {
+    try {
+        # Get the name of the zip file without extension
+        #$zipFileName = [System.IO.Path]::GetFileNameWithoutExtension($zipFile.FullName)
+        
+        # Extract the contents of the zip file to the destination directory
+        Expand-Archive -Path $zipFile.FullName -DestinationPath $destination -ErrorAction Stop
+        
+        # Log success message
+        Write-Log "Extracted $($zipFile.Name) to $destination"
+    } catch {
+        # Log error message if extraction fails
+        Write-Log "Failed to extract $($zipFile.Name): $_.exception.message"
+    }
+}
+
+
+Write-Log "Script execution completed successfully!"
---
+++ engappinstalls.ps1
@@ -0,0 +1,503 @@
+#######################################################################################################################
+###                                                                                                                 ###
+###    Script Name: engappinstalls.ps1                                                                             ###
+###    Script Function: This script is meant to be run within a Custom Image Template deployment within Azure       ###
+###                     Virtual Desktop. This script installs the required software for the Genpact EN Use case     ###
+###                     within Walmart.                                                                             ### 
+###                     from Azure Blob Storage.                                                                    ###
+###                                                                                                                 ###
+###    Script Usage: This template script does not require any parameters at this time, but if required they can    ###
+###                  be added and called as part of the custom image template process.                              ###
+###                                                                                                                 ###        
+###    Script Version: 1.0                                                                                          ###
+###                                                                                                                 ###
+#######################################################################################################################
+
+
+
+
+# Set logging 
+ 
+$LogFilePath = "C:\ImageBuild\engappinstalls.log"
+function Write-Log
+{
+	param (
+		[string]$Message
+	)
+
+	try
+	{
+		# Check if the log file directory exists, create it if not
+		$logDirectory = Split-Path -Path $LogFilePath
+		if (-Not (Test-Path -Path $logDirectory))
+		{
+			New-Item -Path $logDirectory -ItemType Directory
+			Write-Host "Log directory created at: $logDirectory"
+		}
+		# Get current date and time
+		$dateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+		# Log message with date and time to console and file
+		$logMessage = "$dateTime - $Message"
+		Write-Host $logMessage
+		Add-Content -Path $LogFilePath -Value $logMessage
+	}
+	catch
+	{
+		write-host "Having issues creating or adding information to the logfile at $LogFilePath"
+	}
+}
+
+#ForcePoint
+
+try 
+{
+ 
+    Write-Log "Starting the ForcePoint Install"
+	Start-Process -filepath 'C:\ImageBuild\FP-22.06\FORCEPOINT-ONE-ENDPOINT-x64.exe' -ArgumentList '/v" /quiet /norestart"' -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the ForcePoint Software Install."
+    #Start-Sleep -Seconds 300
+}
+ 
+catch 
+{
+ 
+    $ErrorMessage = $_.Exception.message
+    write-log "Error installing ForcePoint-v22.06.5578: $ErrorMessage"
+	Exit 42
+}
+
+#Tanium
+
+try 
+{
+	Write-Log "Starting the Tanium Install"    
+	$source = "C:\ImageBuild\Tanium 7.4.7.1179\tanium-init.dat"
+    Start-Process -FilePath 'C:\ImageBuild\Tanium 7.4.7.1179\TaniumInstall.cmd' -Wait -PassThru -ErrorAction Stop
+    Start-Process -FilePath 'C:\Program Files (x86)\Tanium\Tanium Client\TaniumClient.exe' -ArgumentList "config remove ComputerID" -Wait -ErrorAction Stop
+    Start-Process -FilePath 'C:\Program Files (x86)\Tanium\Tanium Client\TaniumClient.exe' -ArgumentList "config remove RegistrationCount" -Wait -ErrorAction Stop
+    Start-Process -FilePath 'C:\Program Files (x86)\Tanium\Tanium Client\TaniumClient.exe' -ArgumentList "config remove LastGoodServerName" -Wait -ErrorAction Stop
+    #Remove-Item -Path 'C:\Program Files (x86)\Tanium\Tanium Client\Downloads' -Recurse -Force
+    #Remove-Item -Path 'C:\Program Files (x86)\Tanium\Tanium Client\Logs' -Recurse -Force
+    Remove-Item -Path 'C:\Program Files (x86)\Tanium\Tanium Client\Backup' -Recurse -Force
+    Stop-Service 'Tanium Client'
+    Remove-Item -Path 'C:\Program Files (x86)\Tanium\Tanium Client\pki.db' -Force
+    Copy-Item -Path $source -Destination 'C:\Program Files (x86)\Tanium\Tanium Client' 
+	Write-Log "Successfully Completed the Tanium Software Install"   
+
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing ForcePoint-v22.06.5578: $ErrorMessage"
+	Exit 42
+}
+
+#Trellix Proxy Agent
+
+try 
+{
+	Write-Log "Starting the Trellix Proxy Agent Install"	
+	Start-Process -FilePath "C:\ImageBuild\FramePkg581L.exe" -ArgumentList "/install=agent /silent" -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the Trellix Proxy Agent Install"
+
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+  	write-log "Error installing TrellixProxyAgent: $ErrorMessage"
+	Exit 42
+}
+
+#Frameworks
+
+try 
+{
+	Write-Log "Starting the install of Visual C++ Redist x64 Package"
+	Start-Process -FilePath "c:\ImageBuild\Frameworks\VC_redist.x64.exe" -ArgumentList "/quiet /norestart" -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the install of Visual C++ Redist x64 Package"
+}
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing VC_redist.x64.exe: $ErrorMessage"
+	Exit 42
+}
+
+try 
+{
+	Write-Log "Starting the install of Visual C++ Redist x86 Package"	
+	Start-Process -FilePath "c:\ImageBuild\Frameworks\VC_redist.x86.exe" -ArgumentList "/quiet /norestart" -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the install of Visual C++ Redist x64 Package"
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing VC_redist.x86.exe: $ErrorMessage"
+	Exit 42
+}
+
+try 
+{
+	Write-Log "Starting the install of the .Net 3.5 Package"	
+	Add-WindowsCapability -Online -Name NetFx3 -Source "C:\ImageBuild\Frameworks\dotnetfx35.exe"
+	Write-Log "Successfully Completed the install of the .Net 3.5 Package"	
+}
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing dotnetfx35: $ErrorMessage"
+	Exit 42
+}
+
+#Python 3.10.5
+
+try 
+{
+    Write-Log "Starting the install of the Python 3.10.5 Package"	
+    Start-Process -FilePath "C:\ImageBuild\Python\python-3.10.5-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 TARGETDIR=C:\Program Files\Python310" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the Python 3.10.5 Package"	
+    Write-Log "Now installing Python Modules"
+    Start-Process "C:\Program Files\Python310\Scripts\pip.exe" -ArgumentList "install google-cloud-storage" -Wait -ErrorAction Stop
+    Start-Process "C:\Program Files\Python310\Scripts\pip.exe" -ArgumentList "install pandas" -Wait -ErrorAction Stop
+    Start-Process "C:\Program Files\Python310\Scripts\pip.exe" -ArgumentList "install pandas-gbq" -Wait -ErrorAction Stop
+    Start-Process "C:\Program Files\Python310\Scripts\pip.exe" -ArgumentList "install matplotlib" -Wait -ErrorAction Stop
+    Start-Process "C:\Program Files\Python310\Scripts\pip.exe" -ArgumentList "install requests==2.29.0" -Wait -ErrorAction Stop
+    Start-Process "C:\Program Files\Python310\Scripts\pip.exe" -ArgumentList "install gcsfs" -Wait -ErrorAction Stop
+    Start-Process "C:\Program Files\Python310\Scripts\pip.exe" -ArgumentList "install beautifulsoup4" -Wait -ErrorAction Stop
+    Write-Log "Successfully Installed the additional python modules"
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing python-3.10.5: $ErrorMessage"
+    Exit 42
+}
+
+### .Net 5.0 Install ###
+
+try 
+{
+    Write-Log "Starting the install of the .Net 5.0 Package"
+    Start-Process -FilePath "C:\ImageBuild\dotnet5.0.12\windowsdesktop-runtime-5.0.12-win-x64.exe" -ArgumentList "/install /quiet /norestart" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the .Net 5.0 Package"
+}
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing DAX_Studio_ver3.0.8.945: $ErrorMessage"
+    Exit 42
+}
+
+#Java1.8
+
+try 
+{
+    Write-Log "Starting the install of the Java 1.8 Client Package"	
+	Start-Process -FilePath "C:\ImageBuild\Java1.8\Java_1.8.EXE" -ArgumentList "/S /NOREBOOT" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the Java 1.8 Client Package"
+
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing Java 1.8: $ErrorMessage"
+    Exit 42
+}
+
+#Notepad++ v7.7.1
+
+ try 
+{
+    Write-Log "Starting the install of the Notepad++ Package"
+    Start-Process -FilePath "C:\ImageBuild\Notepad++_ver``[7.7.1``]\Deploy-Application.exe" -ArgumentList "Install NonInteractive" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the Notepad++ Package"
+}
+
+catch 
+{
+    $ErrorMessage = $_.Exception.message
+   	write-log "Error installing Notepad++_ver[7.7.1]: $ErrorMessage"
+    Exit 42
+}
+
+#VSCode
+
+try 
+{
+    Write-Log "Starting the install of the VSCode Package"
+    Start-Process -FilePath "C:\ImageBuild\VSCode\VSCodeUserSetup-x64-1.85.1.exe" -ArgumentList "/verysilent /suppressmsgboxes /mergetasks=!runcode" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the VSCode Package"
+}
+
+catch 
+{
+    $ErrorMessage = $_.Exception.message
+   	write-log "Error installing VSCodeUserSetup-x64-1.85.1: $ErrorMessage"
+    Exit 42
+}
+
+#Git
+
+try 
+{
+    Write-Log "Starting the install of the GIT Package"
+    Start-Process -FilePath "C:\ImageBuild\Git\Git-2.33.0-64-bit.exe" -ArgumentList "/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the GIT Package"
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing Git-2.33.0-64-bit: $ErrorMessage"
+    Exit 42
+}
+
+ 
+#MicrosoftEdgeWebview2
+
+try 
+{
+	Write-Log "Starting the install of the Microsoft Edge WebView Package"
+	Start-Process -FilePath "C:\ImageBuild\EdgeWebview2\MicrosoftEdgeWebview2Setup.exe" -ArgumentList '/silent /install' -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the install of the Microsoft Edge WebView Package"
+
+}
+
+catch
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing MicrosoftEdgeWebview2: $ErrorMessage"
+	Exit 42
+}
+
+ #PowerBI
+
+try 
+{
+    Write-Log "Starting the install of the Power BI Package"	
+    Start-Process -FilePath "C:\ImageBuild\PowerBI_ver2.121.762\Deploy-Application.exe" -ArgumentList "Install NonInteractive" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the Power BI Package"	
+
+}
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing SSMS: $ErrorMessage"
+    Exit 42
+}
+ 
+#VSCode
+
+try 
+{
+    Write-Log "Starting the install of the VSCode Package"
+    Start-Process -FilePath "C:\ImageBuild\VSCode\VSCodeUserSetup-x64-1.85.1.exe" -ArgumentList "/verysilent /suppressmsgboxes /mergetasks=!runcode" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the VSCode Package"
+}
+
+catch 
+{
+    $ErrorMessage = $_.Exception.message
+   	write-log "Error installing VSCodeUserSetup-x64-1.85.1: $ErrorMessage"
+    Exit 42
+}
+
+#Putty
+
+try {
+
+	Write-Log "Starting the install of the Putty Package"	
+    Start-Process -FilePath "C:\ImageBuild\Putty\Putty.EXE" -ArgumentList "/S /NOREBOOT" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the Putty Package"
+
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing 7-Zip: $ErrorMessage"
+    Exit 42
+}
+
+#BogleFont
+try 
+{
+	Write-Log "Starting the install of the Bogle Font Package"	
+	Start-Process -FilePath "C:\ImageBuild\BogleFont\BogleFont-1.0.msi" -ArgumentList "/q ALLUSERS=2 /m MSIYXKCO /l* c:\Windows\temp\BogleFonts.log" -Wait -ErrorAction Stop
+	Write-Log "Successfully Completed the install of the Bogle Font Package"
+
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing BogleFont: $ErrorMessage"
+	Exit 42
+}
+
+#IntelliJ
+
+try 
+{
+    Write-Log "Starting the install of the InteliJ Package"
+	Start-Process -FilePath "C:\ImageBuild\IntelliJ_Idea\IntelliJ_IDEA.EXE" -ArgumentList "/S /NOREBOOT" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the InteliJ Package"
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing IntelliJ: $ErrorMessage"
+    Exit 42
+}
+
+#Azul-Zulu_JDK-17_ver17.42.19
+
+try 
+{
+    Write-Log "Starting the install of the Azul-Zulu JDK Package"
+	Start-Process -FilePath "C:\ImageBuild\Azul-Zulu_JDK-17_ver17.42.19\Deploy-Application.exe" -ArgumentList "Install NonInteractive" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the Azul-Zulu JDK Package"
+
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing Azul-Zulu_JDK-17_ver17.42.19: $ErrorMessage"
+    Exit 42
+}
+
+#Powershell7.4.0
+
+try 
+{
+    Write-Log "Starting the install of the PowerShell 7 Package"
+    Start-Process -FilePath "C:\ImageBuild\Powershell7.4.0\PowerShell-7.4.0-win-x64.msi" -ArgumentList "/quiet" -Wait -ErrorAction Stop
+    Write-Log "Successfully Completed the install of the PowerShell 7 Package"
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error installing Powershell7.4.0: $ErrorMessage"
+    Exit 42
+}
+
+
+# PowerShell script to create web shortcuts (.URL files) for Office Web Apps on the desktop for all users with custom icons, error handling, and logging
+
+# Define shortcut locations
+Write-Log "Configuring Desktop Shortcuts for Office Web App Links"
+$desktopPath = [System.Environment]::GetFolderPath('CommonDesktopDirectory')
+
+# Copy icon files to System32 directory
+
+try 
+{
+	write-log "Copying Office icons to System32 directory"
+	Copy-Item "C:\ImageBuild\OfficeIcons" -Recurse -Destination "C:\Windows\System32"
+	write-log "Successfully copied the Office icon files"
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error copying Office icon files: $ErrorMessage"
+    Exit 42
+}
+
+# Define Office Web Apps URLs and their corresponding custom icon paths
+$officeApps = @{
+    "Microsoft Word" = @{
+        "URL" = "https://office.live.com/start/Word.aspx";
+        "Icon" = "C:\Windows\System32\OfficeIcons\word.ico"
+    }
+    "Microsoft Excel" = @{
+        "URL" = "https://office.live.com/start/Excel.aspx";
+        "Icon" = "C:\Windows\System32\OfficeIcons\excel.ico"; # Update path to your custom icon
+    }
+    "Microsoft PowerPoint" = @{
+        "URL" = "https://office.live.com/start/PowerPoint.aspx";
+        "Icon" = "C:\Windows\System32\OfficeIcons\PowerPoint.ico"; # Update path to your custom icon
+    }
+    "Microsoft OneNote" = @{
+        "URL" = "https://www.onenote.com/notebooks";
+        "Icon" = "C:\Windows\System32\OfficeIcons\OneNote.ico"; # Update path to your custom icon
+    }
+    "Microsoft Outlook" = @{
+        "URL" = "https://outlook.wal-mart.com/mail";
+        "Icon" = "C:\Windows\System32\OfficeIcons\outlook.ico"; # Update path to your custom icon
+    }
+}
+
+# Loop through each app and create a web shortcut on the desktop
+Write-Log "Creating Desktop Icons for Office Web Apps"
+foreach ($appName in $officeApps.Keys) {
+    $appDetails = $officeApps[$appName]
+    $url = $appDetails["URL"]
+    $icon = $appDetails["Icon"]
+    $shortcutPath = Join-Path -Path $desktopPath -ChildPath "$appName.url"
+
+    try {
+        $shortcutContent = @"
+[InternetShortcut]
+URL=$url
+IconFile=$icon
+IconIndex=0
+"@
+        $shortcutContent | Out-File -FilePath $shortcutPath
+        Write-Log "Successfully created web shortcut for $appName with custom icon on the desktop for all users."
+    } catch {
+        Write-Log "Failed to create web shortcut for $appName. Error: $_"
+        Exit 42
+    }
+}
+
+Write-Host "Web shortcuts with custom icons created successfully on the desktop for all users."
+Write-Log "Completed creating web shortcuts with custom icons on the desktop for all users."
+
+# Creating additional desktop shortcuts.
+Write-Log "Creating the MS Outlook and File Explorer shortcuts on the desktop for all users."
+try 
+{
+	$publicDesktop = [System.Environment]::GetFolderPath('CommonDesktopDirectory')
+	$outlookPath = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+	$fileExplorerPath = "C:\Windows\explorer.exe"
+	Write-Log "Creating the Outlook Shortcut"
+	$outlookShortcut = $publicDesktop + "\Microsoft Outlook.lnk"
+	$shell = New-Object -ComObject WScript.Shell
+	$shortcut = $shell.CreateShortcut($outlookShortcut)
+	$shortcut.TargetPath = $outlookPath
+	$shortcut.Save()
+	Write-Log "Creating the File Explorer Shortcut"
+	$fileExplorerShortcut = $publicDesktop + "\Windows File Explorer.lnk"
+	$shortcut = $shell.CreateShortcut($fileExplorerShortcut)
+	$shortcut.TargetPath = $fileExplorerPath
+	$shortcut.Save()
+}
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+    write-log "Error creating a shortcut $ErrorMessage"
+    Exit 42
+}
+# Installing CMTrace.exe
+try 
+{
+	write-log "Copying CMTrace to System32 directory"
+	Copy-Item "C:\ImageBuild\CMTrace.exe" -Recurse -Destination "C:\Windows\System32"
+	write-log "Successfully copied the CMTrace.exe to the System32 Directory"
+}
+
+catch 
+{
+	$ErrorMessage = $_.Exception.message
+   	write-log "Error copying CMTrace.exe: $ErrorMessage"
+    Exit 42
+}
